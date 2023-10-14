bd9cd1662e407ce2b03f18d7e0cf9d397d51e8c1
Ahmad Septian <ahmadseptian@Ahmads-MacBook-Pro.local>
2023-10-14 21:43:27 +0700

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const legalEntityRoutes = require('./routes/legalentitiesRouter');
 const durationRoutes = require('./routes/durationsRouter');
 const middlewareLogRequest = require('./middleware/logs');
-const upload = require('./middleware/multer');
+// const upload = require('./middleware/multer');
 
 const PORT = process.env.PORT;
 
@@ -18,11 +18,11 @@ app.use('/assets', express.static('public/images'));
 // Routes
 app.use('/api/v1/legalentities', legalEntityRoutes);
 app.use('/api/v1/durations', durationRoutes);
-app.post('/upload', upload.single('photo'), (req, res) => {
-    res.json({
-        message: "Success upload image"
-    });
-});
+// app.post('/upload', upload.single('photo'), (req, res) => {
+//     res.json({
+//         message: "Success upload image"
+//     });
+// });
 
 app.use((err, req, res, next) => {
     res.json({
